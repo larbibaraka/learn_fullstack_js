c341631a6939e0a30ae6135a2c62429eb92ec663
larbibaraka <laridev@gmail.com>
2018-12-29 11:58:19 +0100

    adding some important files

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log('batarar');
+import config , {nodeEnv} from './config';
+import express from 'express';
+import ApiRouter from './api';
+
+const server = express();
+
+
+//adding the api route
+server.use('/api', ApiRouter);
+//using ejs as a template engine
+server.set('view engine' , 'ejs');
+
+server.get('/', (req, res)=>{
+  res.render('index' , {
+    content : 'assalam alykoum welcome to my page'
+  });
+});
+//serving the static files like css , images ...etc
+server.use(express.static('public'));
+
+server.listen(config.port, ()=>{
+  console.log('hello this is working on port ', config.port );
+});
+
+
+
+ 
